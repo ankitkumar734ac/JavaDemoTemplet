952b0bd48770956376059908dcd2f23681f868e2
Ankit Kumar <kumar.ankit@cloudanalogy.com>
2023-04-26 22:13:09 +0530

    app-info jseon file app-info api

--- src/main/java/com/ca/core/controllers/TestController.java
+++ src/main/java/com/ca/core/controllers/TestController.java
@@ -1,17 +1,23 @@
 package com.ca.core.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -58,6 +64,24 @@ public class TestController {
 		String message = "Public Content.";
 	    ApiResponse response = new ApiResponse(true, "Success", message);
 	    return ResponseEntity.ok().body(response);
+	}
+	@GetMapping("/app-info")
+	public ResponseEntity<?> getPlaylode() {
+		String filename = "app-data.json";
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filename).getFile());
+        System.out.println(classLoader.getResource(filename).getFile());
+        try {
+            // Parse the contents of the JSON file
+            String contents = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+            JSONObject jsonObject = new JSONObject(contents);
+
+            // Return the JSON object as a response
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(jsonObject.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reading file " + filename);
+        }
 	}
 	
 	@GetMapping("/user")
